c832922351c281690b2b26c5ed1b6d4deddd6643
Trent <trentdm@gmail.com>
2014-09-12 08:05:28 -0600

    Cleaned up formatting.

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ var repo = require('./services/repo');
 var getJulian = function(date) {
     var onejan = new Date(date.getFullYear(),0,1);
     return Math.ceil((date - onejan) / 86400000);
-}
+};
 
 var getRequiredCommits = function(){
     var data = canvas.getCanvas(config.input);
@@ -17,29 +17,31 @@ var getRequiredCommits = function(){
         return data[julian];
     else
         return 0;
-}
+};
 
 var appendFile = function() {
     fs.appendFile(config.file, new Date(), [], function (error) {
         if(error) throw error;
     });
-}
+};
 
 var updateCanvas = function() {
-    console.log('updating canvas at ' + new Date())
     try
     {
+        console.log('Updating canvas at ' + new Date());
         var requiredCommits = getRequiredCommits();
+
         for(var i = 0; i < requiredCommits; i++) {
             appendFile();
             repo.addCommit(config.repo_url, "Updated " + config.file);
         }
+
         repo.pushCommits(config.repo_url);
     }
     catch(error){
-        console.log("Error: " + error)
+        console.log("Error: " + error);
     }
-}
+};
 
 //running method
 //repeat(updateCanvas).every(24, 'h').start.now()
